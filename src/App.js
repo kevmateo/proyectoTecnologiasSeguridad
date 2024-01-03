da35f44052dca95c4936cdb48e6e5f6a03c19b72
kevmateo <kevin.cano@epn.edu.ec>
2024-01-03 18:32:06 -0500

    Subir proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="bg-gray-800 h-screen text-white flex justify-center items-center">
+        <div className='w-screen border'>
+          <p className='titulo-proyecto text-3xl font-serif font-bold'>Proyecto Tecnologías de Seguridad</p>
+          <p className='tema-proyecto text-base font-serif'>Sitio web seguro en windows server 2019</p>
+        </div>
+      </div> 
   );
 }
 
